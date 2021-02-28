9088002abe605d9b07ff359f6e05b5d95888630c
Jhappy77 <joel.happ@shaw.ca>
2021-02-27 21:50:20 -0700

    experimentation w backgrounds

--- src/components/sections/HomePage/TestSection.tsx
+++ src/components/sections/HomePage/TestSection.tsx
@@ -2,17 +2,47 @@ import React from 'react';
 import tsu_logo from 'images/logo_white_start.png';
 import './TestSection.css';
 
-function TestSection(){
+function TestSection() {
     return <div className="testSection">
-      <div className="testSection__layout">
-          <div>
-              <h2 className="testSection__header">Hello world.</h2>
-              <p>Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods.</p>
-          </div>
-          <div className="testSection__imsdsg">
-              <img className="testSection__img" src={"https://static.coindesk.com/wp-content/uploads/2021/02/EtkW62iVcAEmi7E-828x628.jpeg"}></img>
-          </div>
-      </div>
+        <div className="testSection__layout">
+            <div>
+                <h2 className="testSection__header">Hello world.</h2>
+                <p>Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods.</p>
+                <div className="testSection__header turquoise">#23CAB5</div>
+                <p className="testSection__header green">green #8FDC63</p>
+            </div>
+            <div className="testSection__imsdsg">
+                <img className="testSection__img" src={"https://1.bp.blogspot.com/-2qME1-9hY2Y/V50IxjIUJcI/AAAAAAABzQI/x0EzDRh2WZEfI3erhRhJsU6aH-SKWETBgCLcB/s1600/Originalni.jpg"}></img>
+            </div>
+        </div>
+        <div className="testSection__part2">
+            <div className="testSection__margin">
+                <div className="testSection__part2grid">
+                    <div>
+                        <h2 className="testSection__header turquoise">Hello world.</h2>
+                        <p className="white">Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods.</p>
+                    </div>
+                    <div className="testSection__imsdsg">
+                        <img className="testSection__img" src={"https://1.bp.blogspot.com/-2qME1-9hY2Y/V50IxjIUJcI/AAAAAAABzQI/x0EzDRh2WZEfI3erhRhJsU6aH-SKWETBgCLcB/s1600/Originalni.jpg"}></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="testSection__part3">
+            <div className="testSection__margin">
+                <div className="testSection__part2grid">
+                    <div>
+                        <h2 className="testSection__header green">Hello world.</h2>
+                        <p className="white">Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods.</p>
+                    </div>
+                    <div className="testSection__imsdsg">
+                        <img className="testSection__img" src={"https://1.bp.blogspot.com/-2qME1-9hY2Y/V50IxjIUJcI/AAAAAAABzQI/x0EzDRh2WZEfI3erhRhJsU6aH-SKWETBgCLcB/s1600/Originalni.jpg"}></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 }
 
